3c37bce4660a938483ce83c7c490b67f58a9e43b
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-05-12 02:44:40 -0300

    EXERCÍCIO 2 - Implemente os casos de teste para a função numbers

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -21,16 +21,6 @@ describe('1 -Testando a função `numbers` ', () => {
   });
 
 });
-/*
-  A função `numbers` recebe um array de tamanho variável e retorna `true` se todos os parâmetros forem do tipo 'number' e `false` caso contrário.
-
-  Parâmetros:
-    - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, 'a']; [].
-  Comportamento:
-    - numbers([2, 3, 4]); // Retorna: true
-    - numbers([2, 'errado', 5]); // Retorna: false
-
-*/
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it("numbers([1, 2, 3, 4, 5]); Retorna: true", ()=>{
@@ -46,7 +36,4 @@ describe('2 - Implemente os casos de teste para a função `numbers`', () => {
     expect(numbers([' '])).toEqual(false);
   });
 });
-  // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-  // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-  // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-  // Escreva um teste em que a função recebe [' '] e retorna false
+ 
