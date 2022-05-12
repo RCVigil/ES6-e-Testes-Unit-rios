d603f188576b9a12870b18544fc71acaa109b513
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-05-12 02:54:14 -0300

    EXERCÍCIO 2 - Implemente os casos de teste para a função numbers.

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -1,39 +1,35 @@
-/* eslint-disable max-len */
-/* eslint-disable no-unused-vars */
-
 const numbers = require('../src/numbers');
 
 describe('1 -Testando a função `numbers` ', () => {
-  it("numbers([1, 2]); // Retorna: true", ()=>{
+  it("numbers([1, 2]); // Retorna: true", () => {
     expect(numbers([1, 2])).toBe(true);
   });
 
-  it("numbers([1, 2, 3, 4, 5]); // Retorna: falso", ()=>{
+  it("numbers([1, 2, 3, 4, 5]); // Retorna: falso", () => {
     expect(numbers([1, 2, 3, 4, 5])).toBe(true);
   });
 
-  it("numbers([1, 2, 'a']); // Retorna: false", ()=>{
+  it("numbers([1, 2, 'a']); // Retorna: false", () => {
     expect(numbers([1, 2, 'a'])).toEqual(false);
   });
 
-  it("numbers([ ]); // Retorna: false", ()=>{
+  it("numbers([ ]); // Retorna: false", () => {
     expect(numbers([2, 'errado', 5])).toEqual(false);
   });
 
 });
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
-  it("numbers([1, 2, 3, 4, 5]); Retorna: true", ()=>{
+  it("numbers([1, 2, 3, 4, 5]); Retorna: true", () => {
     expect(numbers([1, 2, 3, 4, 5])).toBe(true);
   });
-  it("numbers([1, 2, '3', 4, 5]); Retorna: false", ()=>{
+  it("numbers([1, 2, '3', 4, 5]); Retorna: false", () => {
     expect(numbers([1, 2, '3', 4, 5])).toEqual(false);
   });
-  it("numbers([1, 'a', 3]); Retorna: false", ()=>{
+  it("numbers([1, 'a', 3]); Retorna: false", () => {
     expect(numbers([1, 'a', 3])).toEqual(false);
   });
-  it("numbers([' ']); Retorna: false", ()=>{
+  it("numbers([' ']); Retorna: false", () => {
     expect(numbers([' '])).toEqual(false);
   });
 });
- 
